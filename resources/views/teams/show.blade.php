54085153cc4b75f98335060b4cb9576f649ca66a
Sean Lin <hylin827@gmail.com>
2023-12-06 10:47:29 +0800

    調整文字

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -5,7 +5,7 @@
 @section('nba_theme', '您所選取的球隊資料')
 
 @section('nba_contents')
-<h1>顯示單一球員</h1>
+<h1>顯示單一球隊</h1>
 球隊編號：{{ $team->id }}<br/>
 球隊名字：{{ $team->name }}<br/>
 球隊所在城市：{{ $team->city }}<br/>
