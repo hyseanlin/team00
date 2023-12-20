56b3c1791a89f5ca8e42bdc49bdc89f7e4a8229d
Sean Lin <seanlin@gm.lhu.edu.tw>
2023-12-20 11:50:18 +0800

    在不建立FormRequest類別的情況下，為players, teams增加資料驗證與錯誤訊息顯示功能。

--- resources/views/players/edit.blade.php
+++ resources/views/players/edit.blade.php
@@ -5,6 +5,7 @@
 @section('nba_theme', '編輯中的球員')
 
 @section('nba_contents')
+    @include('message.list')
     {!! Form::model($player, ['method'=>'PATCH', 'action'=>['\App\Http\Controllers\PlayersController@update', $player->id]]) !!}
     @include('players.form', ['submitButtonText'=>"更新球員資料"])
     {!! Form::close() !!}
--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -5,6 +5,7 @@
 @section('nba_theme', '建立球隊的表單')
 
 @section('nba_contents')
+    @include('message.list')
     {!! Form::open(['url' => 'teams/store']) !!}
     @include('teams.form', ['submitButtonText'=>'新增球隊資料'])
     {!! Form::close() !!}
